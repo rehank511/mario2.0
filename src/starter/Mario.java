2970fe0ada8556b1f00e6af35a7e266d4c13d19d
jeffr <jeffr@DESKTOP-E1UB3NS>
2018-11-10 11:40:14 -0800

    Fixed Movement.

--- src/starter/Mario.java
+++ src/starter/Mario.java
@@ -1,139 +1,6 @@
-package starter;
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
-import java.awt.*;
-import java.awt.event.*;
 
-import javax.swing.*;
-
-public class Mario extends GraphicsProgram implements ActionListener {
-	public static final int GROUND = 400;
-	private static final int SPEED = 2;
-	private static final int MAXACCEL = 30;
-	public static final int PROGRAM_HEIGHT = 800;
-	public static final int PROGRAM_WIDTH = 1000;
-//	public 
-	public int numSec = 0;
-	
-	private int xCoord;
-	private int yCoord;
-	private int accel=0;
-	private GRect mario = new GRect(100,600,100,100);
-	private GRect floor = new GRect(0,700,80000,GROUND);
-	Timer marioTimer = new Timer(70, this);
-	
-	
-	public void init() {
-		setSize(PROGRAM_WIDTH,PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		   //TODO add what we want to do every two seconds
-		
-		mario.move(0, -SPEED*accel);
-		numSec++;
-		accel++;
-		
-		if(numSec >= 7)
-		{ 
-			
-			mario.move(0, SPEED*accel);
-			if(mario.getY()==600)
-			{
-				accel=0;
-				marioTimer.stop();
-			}
-			
-		}
-		
-		
-		
-	}
-
-	public void run() {
-		add(mario);
-		add(floor);
-		
-		addKeyListeners();
-		addActionListeners();
-		
-	}
-	
-	public void moveRight()
-	{
-		while(accel<MAXACCEL)
-		{
-			accel++;
-		}
-		mario.move(SPEED*accel, 0);
-	}
-	
-	public void moveLeft()
-	{
-		while(accel<MAXACCEL)
-		{
-			accel++;
-		}
-		mario.move(-SPEED*accel,0);
-	}
-	
-	public void moveUp()
-	{
-//		for(int i =0; i<4;i++)
-//		{
-//			mario.move(0, -SPEED*i);
-//		}
-//		mario.setLocation(mario.getX(), mario.getY()-SPEED*accel);
-//		for(int i = 0; i<4;i++)
-//		{
-//			mario.move(0, SPEED*i);
-//		}
-		marioTimer.start();
-		
-	}
-	
-	public boolean marioOnGround()
-	{
-		
-		if(mario.getY()!=floor.getY())
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-		
-	}
-	
-	@Override
-	public void keyPressed(KeyEvent e)
-	{
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
-			moveRight();
-		}
-		else if(e.getKeyCode()==KeyEvent.VK_LEFT)
-		{
-			moveLeft();
-		}
-		else if(e.getKeyCode()==KeyEvent.VK_UP)
-		{
-			moveUp();
-		}
-		
-//		else
-//		{
-//			return;
-//		}
-	}
-
-	
-}
-=======
 //Jeffrey
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 package starter;
 
 import acm.graphics.*;
@@ -153,14 +20,12 @@ public class Mario extends GraphicsProgram {
 	Platform[][] p;
 	Platform[] P;
 
-	public double qq = 5;
-	public int x = 100, y = 750, w = 50, h = 50, q = 3;
+	public int x = 100, y = 750, w = 50, h = 50, s = 3; // Mario parameters
 	public int dh = 0, dw = 0;
-	public double maxdw = 5, maxdh = 15, walksd = 1, slowsd = .1, jumpsd = 22, fallsd = 1;
-	boolean onground = false;
+	public double maxdw = 5, maxdh = 15, walksd = 1, slowsd = 1, jumpsd = 15, fallsd = 1; // Mario movement speed
 
 	public GRect Mario;
-	public GRect t, b, l, r;
+	public GRect t, b, l, r; // t = top, b = bottom, l = left, r = right
 
 	public Mario() {
 		Mario = new GRect(0, 0, 0, 0);
@@ -191,13 +56,13 @@ public class Mario extends GraphicsProgram {
 	public boolean collision, R, L, T, B;
 
 	public void run() {
-		InitilizeMario(x, y, w, h, q);
+		InitilizeMario(x, y, w, h, s);
 		add(Mario);
 		add(t);
 		add(b);
 		add(r);
 		add(l);
-		
+
 		p = new Platform[50][5];
 		for (int a = 0; a < p.length; a++)
 			for (int i = 0; i < p[0].length; i++) {
@@ -211,7 +76,7 @@ public class Mario extends GraphicsProgram {
 
 		for (int a = 0; a < p.length; a++) {
 			for (int i = 0; i < p[0].length; i++) {
-				p[a][i].InitilizePlatform(a*400 + 400 + i * 50, 600 - a%3 * 200, 50, 50, 3);
+				p[a][i].InitilizePlatform(a * 400 + 400 + i * 50, 600 - a % 3 * 200, 50, 50, 3);
 			}
 		}
 
@@ -239,11 +104,14 @@ public class Mario extends GraphicsProgram {
 			add(P[i].l);
 		}
 
-		Timer t = new Timer(1, this);
+		Timer t = new Timer(10, this);
 		t.start();
 
 		addKeyListeners();
 	}
+
+	int i = 0;
+	int onground = 0;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -251,6 +119,11 @@ public class Mario extends GraphicsProgram {
 			collision(p[a]);
 		}
 		collision(P);
+
+		System.out.print(onground);
+		if (onground > 0)
+			collideT = true;
+
 		if (Mario.getX() < 0 || Mario.getX() > 600) {
 			if (Mario.getX() < 0) {
 				dw = 0;
@@ -269,70 +142,112 @@ public class Mario extends GraphicsProgram {
 				} else
 					moveMario(dw, dh);
 			}
-		} else
+		} else 
 			moveMario(dw, dh);
-		if (dh < maxdh)
-			dh += fallsd;
-		System.out.print(dh);
+
+		if (i % 10 == 0)
+			if (Moving == false) {
+				if (dw > 0)
+					dw -= slowsd;
+				if (dw < 0)
+					dw += slowsd;
+			}
+		if (i % 10 == 0)
+		if (MovingLeft == true) {
+			if (dw > -maxdw)
+				dw -= walksd;
+			if (dw >= 0)
+				dw -= walksd;
+		}
+		if (i % 10 == 0)
+		if (MovingRight == true) {
+			if (dw < maxdw)
+				dw += walksd;
+			if (dw <= 0)
+				dw += walksd;
+		}
+
+		i++;
+		if (i % 2 == 0)
+			if (i % 1 == 0)
+				if (dh <= maxdh)
+					dh += fallsd;
+
+		onground = 0;
 	}
 
 	public void collision(Platform[] p) {
+		collideT = false;
+		collideB = false;
+		collideL = false;
+		collideR = false;
 		for (int i = 0; i < p.length; i++) {
 			if ((b.getBounds()).intersects(p[i].t.getBounds())) {
-				onground = true;
 				collideT = true;
+				onground++;
 				if (dh > 0)
 					dh = 0;
 				moveMario(0, p[i].F.getY() - Mario.getY() - h);
-			} else
-				collideT = false;
+			}
+
 			if ((t.getBounds()).intersects(p[i].b.getBounds())) {
 				collideB = true;
 				if (dh < 0)
 					dh = 0;
 				moveMario(0, p[i].t.getY() - Mario.getY() + p[i].F.getHeight());
-			} else
-				collideB = false;
+			}
 			if ((r.getBounds()).intersects(p[i].l.getBounds())) {
 				collideL = true;
 				if (dw > 0)
 					dw = 0;
 				moveMario(p[i].F.getX() - Mario.getX() - Mario.getWidth(), 0);
-			} else
-				collideL = false;
+			}
 			if ((l.getBounds()).intersects(p[i].r.getBounds())) {
 				collideR = true;
 				if (dw < 0)
 					dw = 0;
 				moveMario(p[i].F.getX() + p[i].F.getWidth() - Mario.getX(), 0);
-			} else
-				collideR = false;
+			}
+		}
+	}
+
+	boolean Moving = false;
+	boolean MovingRight = false;
+	boolean MovingLeft = false;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		Moving = true;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			MovingRight = true;
+			if (dw < maxdw)
+				dw += walksd;
+			if (dw <= 0)
+				dw += walksd;
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			MovingLeft = true;
+			if (dw > -maxdw)
+				dw -= walksd;
+			if (dw >= 0)
+				dw -= walksd;
+		} else if (e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_UP) {
+			if (collideT) {
+				dh -= jumpsd;
+			}
 		}
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyReleased(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			if (dw < maxdw)
-				dw += walksd;
-			if (dw <= 0)
-				dw += walksd * 2;
-		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			if (dw > -maxdw)
-				dw -= walksd;
-			if (dw >= 0)
-				dw -= walksd * 2;
-		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			if (dw > 0)
-				dw -= walksd;
-			if (dw < 0)
-				dw += walksd;
-		} else if (e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_UP) {
-			if (dh == 1 && onground) {
-				dh -= jumpsd;
-				onground = false;
-			}
+			Moving = false;
+			MovingRight = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			Moving = false;
+			MovingLeft = false;
 		}
 	}
 }
->>>>>>> branch 'master' of https://github.com/comp55-fall18/group-project-cloud9.git
+//Jeffrey
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------

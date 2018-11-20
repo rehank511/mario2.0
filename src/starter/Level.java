dfc956a4df2b10e6e063675807d5f45bfd65dc67
abdimanapov <b_abdimanapov@u.pacific.edu>
2018-11-19 21:38:08 -0800

    The whole level code is in separate class

--- src/starter/Level.java
+++ src/starter/Level.java
@@ -14,18 +14,24 @@ import java.util.TimerTask;
 import javax.swing.Timer;
 
 public class Level extends GraphicsProgram{
-	private Platform[][] platform;
-	private Platform[] Ground;
-	
-	private Platform[][] Pipe;
+	public Platform[][] levelPlatform;
+	public Platform[] Ground;
+	public Platform[][] Pipe;
 
 	
 	public void level1()
 	{
-		platform = new Platform[50][10];
-		for (int a = 0; a < platform.length; a++)
-			for (int i = 0; i < platform[0].length; i++) {
-				platform[a][i] = new Platform();
+		GImage background = new GImage("bg.png", 0, 0);
+		background.setSize(850, 600);
+		add(background);
+		GImage ground = new GImage("ground.png", 0, 600);
+		ground.setSize(850, 100);
+		add(ground);
+		
+		levelPlatform = new Platform[50][10];
+		for (int a = 0; a < levelPlatform.length; a++)
+			for (int i = 0; i < levelPlatform[0].length; i++) {
+				levelPlatform[a][i] = new Platform();
 			}
 		Ground = new Platform[10];
 		for (int i = 0; i < Ground.length; i++) {
@@ -38,65 +44,64 @@ public class Level extends GraphicsProgram{
 			for (int i = 0; i < Pipe[0].length; i++) {
 				Pipe[a][i] = new Platform();
 			}
-		platform[0][0].InitilizePlatform(400,400,50,50,3);
-		platform[0][1].InitilizePlatform(600,400,50,50,3);
-		platform[0][2].InitilizePlatform(650,400,50,50,3);
-		platform[0][3].InitilizePlatform(700,400,50,50,3);
-		platform[0][4].InitilizePlatform(750,400,50,50,3);
-		platform[0][5].InitilizePlatform(800,400,50,50,3);
-		platform[0][6].InitilizePlatform(850,400,50,50,3);
-		platform[0][7].InitilizePlatform(700,200,50,50,3);
 		
-		platform[1][0].InitilizePlatform(3400,400,50,50,3);
-		platform[1][1].InitilizePlatform(3500,200,50,50,3);
-		platform[1][2].InitilizePlatform(3550,200,50,50,3);
-		platform[1][3].InitilizePlatform(3600,200,50,50,3);
-		platform[1][4].InitilizePlatform(3650,200,50,50,3);
-		platform[1][5].InitilizePlatform(3700,200,50,50,3);
-		platform[1][6].InitilizePlatform(3750,200,50,50,3);
-		platform[1][7].InitilizePlatform(3800,200,50,50,3);
+		levelPlatform[0][0].InitilizePlatform(400,400,50,50,3);
+		levelPlatform[0][1].InitilizePlatform(600,400,50,50,3);
+		levelPlatform[0][2].InitilizePlatform(650,400,50,50,3);
+		levelPlatform[0][3].InitilizePlatform(700,400,50,50,3);
+		levelPlatform[0][4].InitilizePlatform(750,400,50,50,3);
+		levelPlatform[0][5].InitilizePlatform(800,400,50,50,3);
+		levelPlatform[0][6].InitilizePlatform(850,400,50,50,3);
+		levelPlatform[0][7].InitilizePlatform(700,200,50,50,3);
 		
-		platform[2][0].InitilizePlatform(4000,200,50,50,3);
-		platform[2][1].InitilizePlatform(4100,200,50,50,3);
-		platform[2][2].InitilizePlatform(4150,200,50,50,3);
-		platform[2][3].InitilizePlatform(4200,200,50,50,3);
-		platform[2][4].InitilizePlatform(4250,200,50,50,3);
-		platform[2][5].InitilizePlatform(4250,400,50,50,3);
+		levelPlatform[1][0].InitilizePlatform(3400,400,50,50,3);
+		levelPlatform[1][1].InitilizePlatform(3500,200,50,50,3);
+		levelPlatform[1][2].InitilizePlatform(3550,200,50,50,3);
+		levelPlatform[1][3].InitilizePlatform(3600,200,50,50,3);
+		levelPlatform[1][4].InitilizePlatform(3650,200,50,50,3);
+		levelPlatform[1][5].InitilizePlatform(3700,200,50,50,3);
+		levelPlatform[1][6].InitilizePlatform(3750,200,50,50,3);
+		levelPlatform[1][7].InitilizePlatform(3800,200,50,50,3);
 		
-		platform[3][0].InitilizePlatform(4800,400,50,50,3);
-		platform[3][1].InitilizePlatform(4850,400,50,50,3);
+		levelPlatform[2][0].InitilizePlatform(4000,200,50,50,3);
+		levelPlatform[2][1].InitilizePlatform(4100,200,50,50,3);
+		levelPlatform[2][2].InitilizePlatform(4150,200,50,50,3);
+		levelPlatform[2][3].InitilizePlatform(4200,200,50,50,3);
+		levelPlatform[2][4].InitilizePlatform(4250,200,50,50,3);
+		levelPlatform[2][5].InitilizePlatform(4250,400,50,50,3);
 		
-		platform[4][0].InitilizePlatform(5400,400,50,50,3);
-		platform[4][1].InitilizePlatform(5600,400,50,50,3);
-		platform[4][2].InitilizePlatform(5800,400,50,50,3);
+		levelPlatform[3][0].InitilizePlatform(4800,400,50,50,3);
+		levelPlatform[3][1].InitilizePlatform(4850,400,50,50,3);
+		
+		levelPlatform[4][0].InitilizePlatform(5400,400,50,50,3);
+		levelPlatform[4][1].InitilizePlatform(5600,400,50,50,3);
+		levelPlatform[4][2].InitilizePlatform(5800,400,50,50,3);
 		
 		
-		platform[5][0].InitilizePlatform(6200,200,50,50,3);
-		platform[5][1].InitilizePlatform(6250,200,50,50,3);
-		platform[5][2].InitilizePlatform(6300,200,50,50,3);
-		platform[5][3].InitilizePlatform(6350,200,50,50,3);
+		levelPlatform[5][0].InitilizePlatform(6200,200,50,50,3);
+		levelPlatform[5][1].InitilizePlatform(6250,200,50,50,3);
+		levelPlatform[5][2].InitilizePlatform(6300,200,50,50,3);
+		levelPlatform[5][3].InitilizePlatform(6350,200,50,50,3);
+		levelPlatform[5][4].InitilizePlatform(6600,200,50,50,3);
+		levelPlatform[5][5].InitilizePlatform(6650,200,50,50,3);
+		levelPlatform[5][6].InitilizePlatform(6700,200,50,50,3);
+		levelPlatform[5][7].InitilizePlatform(6750,200,50,50,3);
+		levelPlatform[5][8].InitilizePlatform(6650,400,50,50,3);
+		levelPlatform[5][9].InitilizePlatform(6700,400,50,50,3);
 		
-		platform[5][4].InitilizePlatform(6600,200,50,50,3);
-		platform[5][5].InitilizePlatform(6650,200,50,50,3);
-		platform[5][6].InitilizePlatform(6700,200,50,50,3);
-		platform[5][7].InitilizePlatform(6750,200,50,50,3);
+		levelPlatform[6][0].InitilizePlatform(6700,400,50,50,3);
+		levelPlatform[6][1].InitilizePlatform(6700,400,50,50,3);
+		levelPlatform[6][2].InitilizePlatform(7300,400,50,50,3);
+		levelPlatform[6][3].InitilizePlatform(7350,400,50,50,3);
 		
-		platform[5][8].InitilizePlatform(6650,400,50,50,3);
-		platform[5][9].InitilizePlatform(6700,400,50,50,3);
-		
-		platform[6][0].InitilizePlatform(6700,400,50,50,3);
-		platform[6][1].InitilizePlatform(6700,400,50,50,3);
-		platform[6][2].InitilizePlatform(7300,400,50,50,3);
-		platform[6][3].InitilizePlatform(7350,400,50,50,3);
-		
-		platform[7][0].InitilizePlatform(8000,550,50,50,3);
-		platform[7][1].InitilizePlatform(8050,500,50,50,3);
-		platform[7][2].InitilizePlatform(8100,450,50,50,3);
-		platform[7][3].InitilizePlatform(8150,400,50,50,3);
-		platform[7][4].InitilizePlatform(8200,350,50,50,3);
-		platform[7][5].InitilizePlatform(8250,300,50,50,3);
-		platform[7][6].InitilizePlatform(8300,250,50,50,3);
-		platform[7][7].InitilizePlatform(8350,250,50,50,3);
+		levelPlatform[7][0].InitilizePlatform(8000,550,50,50,3);
+		levelPlatform[7][1].InitilizePlatform(8050,500,50,50,3);
+		levelPlatform[7][2].InitilizePlatform(8100,450,50,50,3);
+		levelPlatform[7][3].InitilizePlatform(8150,400,50,50,3);
+		levelPlatform[7][4].InitilizePlatform(8200,350,50,50,3);
+		levelPlatform[7][5].InitilizePlatform(8250,300,50,50,3);
+		levelPlatform[7][6].InitilizePlatform(8300,250,50,50,3);
+		levelPlatform[7][7].InitilizePlatform(8350,250,50,50,3);
 		
 		Ground[0].InitilizePlatform(0, 600,2800, 200, 3);
 		Ground[1].InitilizePlatform(3000, 600,800, 200, 3);
@@ -109,30 +114,10 @@ public class Level extends GraphicsProgram{
 		Pipe[0][4].InitilizePlatform(7100, 540, 60, 60, 3);
 		Pipe[0][5].InitilizePlatform(7900, 540, 60, 60, 3);
 		
-		for (int a = 0; a < platform.length; a++) {
-			for (int i = 0; i < platform[0].length; i++) {
-				platform[a][i].getGround().setColor(new Color(212, 212, 212));
-				add(platform[a][i].getGround());
-
-			}
-		}
-
-		for (int i = 0; i < Ground.length; i++) {
-			Ground[i].getGround().setColor(new Color(212, 212, 212));
-			add(Ground[i].getGround());
-
-		}
-
-		for (int a = 0; a < Pipe.length; a++) {
-			for (int i = 0; i < Pipe[0].length; i++) {
-
-				Pipe[a][i].getGround().setColor(new Color(212, 212, 212));
-				add(Pipe[a][i].getGround());
-
-
-			}
-		}
+		
 	}
+	
+	
 	
 
 	public void run()
--- src/starter/Mario.java
+++ src/starter/Mario.java
@@ -19,10 +19,10 @@ import java.util.TimerTask;
 import javax.swing.Timer;
 
 public class Mario extends GraphicsProgram {
-	private Platform[][] platform;
-	private Platform[] Ground;
-	private Level level = new Level();
-	private Platform[][] Pipe;
+//	private Platform[][] platform;
+//	private Platform[] Ground;
+	private Level level = new Level();;
+//	private Platform[][] Pipe;
 	private Enemies[] Goomba;
 	
 	public static int  WIDTH = 50, HEIGHT = 50;
@@ -33,8 +33,10 @@ public class Mario extends GraphicsProgram {
 
 	public int TimerCount = global.TimerCount, onground = 0;
 	boolean Moving = false, movingLeft = false, movingRight = false;
+	
 	private static final int PROGRAM_WIDTH = 850;
 	private static final int PROGRAM_HEIGHT = 650;
+	
 	public boolean collideRight, collideLeft, collideTop, collideBottom;
 	public boolean collision, Right, Left, Top, Bottom;
 
@@ -75,8 +77,7 @@ public class Mario extends GraphicsProgram {
 		Marioleft.move(x, y);
 	}
 
-//	public boolean global.collideRight, collideLeft, collideTop, collideBottom;
-//	public boolean collision, Right, Left, Top, Bottom;
+
 
 	public void marioGraphics(){
 		GImage background = new GImage("bg.png", 0, 0);
@@ -96,29 +97,34 @@ public class Mario extends GraphicsProgram {
 		
 		marioGraphics();
 		
-		level1();
 		
-		gumbaSpot();
+		level.level1();
+		
+		levelSpawn();
+		
+		gumbaSpawn();
 
 		Timer t = new Timer(10, this);
 		t.start();
 		addKeyListeners();
 	}
+	
+	
 
 	// is called after every milisecond and moves the mario and the platform
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		for (int a = 0; a < platform.length; a++) {
-			collision(platform[a]);
+		for (int a = 0; a < level.levelPlatform.length; a++) {
+			collision(level.levelPlatform[a]);
 
 		}
 
 		collision(Goomba);
-		collision(Ground);
+		collision(level.Ground);
 		////
-		for (int a = 0; a < Pipe.length; a++) {
-			collision(Pipe[a]);
+		for (int a = 0; a < level.Pipe.length; a++) {
+			collision(level.Pipe[a]);
 		}
 
 		if (onground > 0)
@@ -136,18 +142,18 @@ public class Mario extends GraphicsProgram {
 			} else if (Mario.getX() > 500) {
 				if (global.horizVelocity > 0) {
 					// moves the platform ground and pipe to left as Mario moves to the right
-					for (int a = 0; a < platform.length; a++) {
-						for (int i = 0; i < platform[0].length; i++) {
-							platform[a][i].movePlatform(-global.horizVelocity, 0);
+					for (int a = 0; a < level.levelPlatform.length; a++) {
+						for (int i = 0; i < level.levelPlatform[0].length; i++) {
+							level.levelPlatform[a][i].movePlatform(-global.horizVelocity, 0);
 
 						}
 					}
-					for (int i = 0; i < Ground.length; i++) {
-						Ground[i].movePlatform(-global.horizVelocity, 0);
+					for (int i = 0; i < level.Ground.length; i++) {
+						level.Ground[i].movePlatform(-global.horizVelocity, 0);
 					}
-					for (int a = 0; a < Pipe.length; a++) {
-						for (int i = 0; i < Pipe[0].length; i++) {
-							Pipe[a][i].movePlatform(-global.horizVelocity, 0);
+					for (int a = 0; a < level.Pipe.length; a++) {
+						for (int i = 0; i < level.Pipe[0].length; i++) {
+							level.Pipe[a][i].movePlatform(-global.horizVelocity, 0);
 						}
 					}
 					for (int i = 0; i < Goomba.length; i++) {
@@ -175,13 +181,13 @@ public class Mario extends GraphicsProgram {
 					} else {
 						Goomba[i].moveGoomba(-1, 0);
 					}
-					for (int a = 0; a < platform.length; a++) {
-						Goomba[i].collisionGoomba(platform[a]);
+					for (int a = 0; a < level.levelPlatform.length; a++) {
+						Goomba[i].collisionGoomba(level.levelPlatform[a]);
 					}
-					Goomba[i].collisionGoomba(Ground);
+					Goomba[i].collisionGoomba(level.Ground);
 					Goomba[i].collisionGoomba(Goomba, i);
-					for (int a = 0; a < Pipe.length; a++) {
-						Goomba[i].collisionGoomba(Pipe[a]);
+					for (int a = 0; a < level.Pipe.length; a++) {
+						Goomba[i].collisionGoomba(level.Pipe[a]);
 					}
 					if (TimerCount % 500 == 0)
 						Goomba[i].changeGoombaDirection();
@@ -373,121 +379,8 @@ public class Mario extends GraphicsProgram {
 		Mariobottom = mariobottom;
 	}
 	
-	public void level1()
-	{
-		platform = new Platform[50][10];
-		for (int a = 0; a < platform.length; a++)
-			for (int i = 0; i < platform[0].length; i++) {
-				platform[a][i] = new Platform();
-			}
-		Ground = new Platform[10];
-		for (int i = 0; i < Ground.length; i++) {
-			Ground[i] = new Platform();
 
-		}
-
-		Pipe = new Platform[50][10];
-		for (int a = 0; a < Pipe.length; a++)
-			for (int i = 0; i < Pipe[0].length; i++) {
-				Pipe[a][i] = new Platform();
-			}
-		platform[0][0].InitilizePlatform(400,400,50,50,3);
-		platform[0][1].InitilizePlatform(600,400,50,50,3);
-		platform[0][2].InitilizePlatform(650,400,50,50,3);
-		platform[0][3].InitilizePlatform(700,400,50,50,3);
-		platform[0][4].InitilizePlatform(750,400,50,50,3);
-		platform[0][5].InitilizePlatform(800,400,50,50,3);
-		platform[0][6].InitilizePlatform(850,400,50,50,3);
-		platform[0][7].InitilizePlatform(700,200,50,50,3);
-		
-		platform[1][0].InitilizePlatform(3400,400,50,50,3);
-		platform[1][1].InitilizePlatform(3500,200,50,50,3);
-		platform[1][2].InitilizePlatform(3550,200,50,50,3);
-		platform[1][3].InitilizePlatform(3600,200,50,50,3);
-		platform[1][4].InitilizePlatform(3650,200,50,50,3);
-		platform[1][5].InitilizePlatform(3700,200,50,50,3);
-		platform[1][6].InitilizePlatform(3750,200,50,50,3);
-		platform[1][7].InitilizePlatform(3800,200,50,50,3);
-		
-		platform[2][0].InitilizePlatform(4000,200,50,50,3);
-		platform[2][1].InitilizePlatform(4100,200,50,50,3);
-		platform[2][2].InitilizePlatform(4150,200,50,50,3);
-		platform[2][3].InitilizePlatform(4200,200,50,50,3);
-		platform[2][4].InitilizePlatform(4250,200,50,50,3);
-		platform[2][5].InitilizePlatform(4250,400,50,50,3);
-		
-		platform[3][0].InitilizePlatform(4800,400,50,50,3);
-		platform[3][1].InitilizePlatform(4850,400,50,50,3);
-		
-		platform[4][0].InitilizePlatform(5400,400,50,50,3);
-		platform[4][1].InitilizePlatform(5600,400,50,50,3);
-		platform[4][2].InitilizePlatform(5800,400,50,50,3);
-		
-		
-		platform[5][0].InitilizePlatform(6200,200,50,50,3);
-		platform[5][1].InitilizePlatform(6250,200,50,50,3);
-		platform[5][2].InitilizePlatform(6300,200,50,50,3);
-		platform[5][3].InitilizePlatform(6350,200,50,50,3);
-		
-		platform[5][4].InitilizePlatform(6600,200,50,50,3);
-		platform[5][5].InitilizePlatform(6650,200,50,50,3);
-		platform[5][6].InitilizePlatform(6700,200,50,50,3);
-		platform[5][7].InitilizePlatform(6750,200,50,50,3);
-		
-		platform[5][8].InitilizePlatform(6650,400,50,50,3);
-		platform[5][9].InitilizePlatform(6700,400,50,50,3);
-		
-		platform[6][0].InitilizePlatform(6700,400,50,50,3);
-		platform[6][1].InitilizePlatform(6700,400,50,50,3);
-		platform[6][2].InitilizePlatform(7300,400,50,50,3);
-		platform[6][3].InitilizePlatform(7350,400,50,50,3);
-		
-		platform[7][0].InitilizePlatform(8000,550,50,50,3);
-		platform[7][1].InitilizePlatform(8050,500,50,50,3);
-		platform[7][2].InitilizePlatform(8100,450,50,50,3);
-		platform[7][3].InitilizePlatform(8150,400,50,50,3);
-		platform[7][4].InitilizePlatform(8200,350,50,50,3);
-		platform[7][5].InitilizePlatform(8250,300,50,50,3);
-		platform[7][6].InitilizePlatform(8300,250,50,50,3);
-		platform[7][7].InitilizePlatform(8350,250,50,50,3);
-		
-		Ground[0].InitilizePlatform(0, 600,2800, 200, 3);
-		Ground[1].InitilizePlatform(3000, 600,800, 200, 3);
-		Ground[2].InitilizePlatform(4000, 600,7000, 200, 3);
-		
-		Pipe[0][0].InitilizePlatform(1000, 540, 60, 60, 3);
-		Pipe[0][1].InitilizePlatform(1400, 520, 60, 80, 3);
-		Pipe[0][2].InitilizePlatform(1800, 500, 60, 100, 3);
-		Pipe[0][3].InitilizePlatform(2200, 480, 60, 120, 3);
-		Pipe[0][4].InitilizePlatform(7100, 540, 60, 60, 3);
-		Pipe[0][5].InitilizePlatform(7900, 540, 60, 60, 3);
-		
-		for (int a = 0; a < platform.length; a++) {
-			for (int i = 0; i < platform[0].length; i++) {
-				platform[a][i].getGround().setColor(new Color(212, 212, 212));
-				add(platform[a][i].getGround());
-
-			}
-		}
-
-		for (int i = 0; i < Ground.length; i++) {
-			Ground[i].getGround().setColor(new Color(212, 212, 212));
-			add(Ground[i].getGround());
-
-		}
-
-		for (int a = 0; a < Pipe.length; a++) {
-			for (int i = 0; i < Pipe[0].length; i++) {
-
-				Pipe[a][i].getGround().setColor(new Color(212, 212, 212));
-				add(Pipe[a][i].getGround());
-
-
-			}
-		}
-	}
-	
-	public void gumbaSpot()
+	public void gumbaSpawn()
 	{
 		Goomba = new Enemies[30];
 		for (int i = 0; i < Goomba.length; i++) {
@@ -499,6 +392,33 @@ public class Mario extends GraphicsProgram {
 			add(Goomba[i].getGoombaImg());
 		}
 
+	}
+	
+	public void levelSpawn()
+	{
+		for (int a = 0; a < level.levelPlatform.length; a++) {
+			for (int i = 0; i < level.levelPlatform[0].length; i++) {
+				level.levelPlatform[a][i].getGround().setColor(new Color(212, 212, 212));
+				add(level.levelPlatform[a][i].getGround());
+
+			}
+		}
+
+		for (int i = 0; i < level.Ground.length; i++) {
+			level.Ground[i].getGround().setColor(new Color(212, 212, 212));
+			add(level.Ground[i].getGround());
+
+		}
+
+		for (int a = 0; a < level.Pipe.length; a++) {
+			for (int i = 0; i < level.Pipe[0].length; i++) {
+
+				level.Pipe[a][i].getGround().setColor(new Color(212, 212, 212));
+				add(level.Pipe[a][i].getGround());
+
+
+			}
+		}
 	}
 }
 

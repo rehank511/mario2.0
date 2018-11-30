b8a766fc7b4fa7fb4119cb12e62365e8e76ed7a3
jeffr <jeffr@DESKTOP-E1UB3NS>
2018-11-29 16:44:19 -0800

    Added Custom Level Editor. Testing Phase. No known bugs.

--- src/starter/Enemies.java
+++ src/starter/Enemies.java
@@ -9,7 +9,9 @@ import java.awt.event.KeyListener;
 
 public class Enemies extends GraphicsProgram {
 	private GRect Top, Bottom, Left, Right;
-	private GImage GoombaImg;
+	private GRect GoombaImg;
+	private GImage GoombaImgRight;
+	private GImage GoombaImgLeft;
 	private boolean goombaDead = false;
 	private int goombaCollideSide = 0, goombaCollideBottom = 0;
 	private int Moving = 0;
@@ -44,8 +46,16 @@ public class Enemies extends GraphicsProgram {
 		return goombaCollideBottom;
 	}
 
-	public GImage getGoombaImg() {
+	public GRect getGoombaImg() {
 		return GoombaImg;
+	}
+	
+	public GImage getGoombaImgRight() {
+		return GoombaImgRight;
+	}
+	
+	public GImage getGoombaImgLeft() {
+		return GoombaImgLeft;
 	}
 
 	public GRect getTop() {
@@ -68,44 +78,50 @@ public class Enemies extends GraphicsProgram {
 		return goombaDead;
 	}
 
-	public void animateGoomba(int i) {
-		if (i % 2 == 0) {
-			GoombaImg.setImage("GoombaRight.png");
-			GoombaImg.setSize(50, 50);
-		} else {
-			GoombaImg.setImage("GoombaLeft.png");
-			GoombaImg.setSize(50, 50);
-		}
-	}
-
 	public Enemies() {
 		Top = new GRect(0, 0, 0, 0);
 		Bottom = new GRect(0, 0, 0, 0);
 		Left = new GRect(0, 0, 0, 0);
 		Right = new GRect(0, 0, 0, 0);
+		GoombaImg = new GRect(0, 0, 0, 0);
+		GoombaImg.setSize(0, 0);
+		goombaDead = true;
 	}
 
 	public void InitilizeGoomba(int x, int y, int w, int h, int q) {
-		GoombaImg = new GImage("GoombaRight.png", 0, 0);
+		GoombaImg = new GRect(0, 0, 0, 0);
 		GoombaImg.setSize(0, 0);
 		GoombaImg.setBounds(x, y, w, h);
+		GoombaImg.setVisible(false);
 		Top.setBounds(x + q, y, w - 2 * q, h / 2);
 		Bottom.setBounds(x + q, y + h / 2, w - 2 * q, h / 2);
 		Left.setBounds(x, y + q, q, h - 2 * q);
 		Right.setBounds(x + w - q, y + q, q, h - 2 * q);
+		GoombaImgLeft = new GImage("GoombaLeft.png", 0, 0);
+		GoombaImgRight= new GImage("GoombaRight.png", 0, 0);
+		GoombaImgRight.setBounds(GoombaImg.getX(), GoombaImg.getY(), 50, 50);
+		GoombaImgLeft.setBounds(GoombaImg.getX(), GoombaImg.getY(), 50, 50);
+		goombaDead = false;
 	}
 
 	public void DeleteGoomba() {
-		goombaDead = true;
 		GoombaImg.setSize(50, 15);
 		GoombaImg.move(0, 35);
+		GoombaImgRight.setSize(50, 15);
+		GoombaImgRight.move(0, 35);
+		GoombaImgLeft.setSize(50, 15);
+		GoombaImgLeft.move(0, 35);
+		if (goombaDead)
 		Top = new GRect(0, 0, 0, 0);
 		Bottom = new GRect(0, 0, 0, 0);
 		Left = new GRect(0, 0, 0, 0);
 		Right = new GRect(0, 0, 0, 0);
+		goombaDead = true;
 	}
 
 	public void moveGoomba(double x, double y) {
+		GoombaImgRight.move(x, y);
+		GoombaImgLeft.move(x, y);
 		GoombaImg.move(x, y);
 		Top.move(x, y);
 		Bottom.move(x, y);
@@ -125,7 +141,7 @@ public class Enemies extends GraphicsProgram {
 			}
 			if ((Left.getBounds()).intersects(p[i].getRight().getBounds())) {
 				goombaCollideSide++;
-				moveGoomba(p[i].getGround().getX() + p[i].getGround().getWidth() - getGoombaImg().getX(), 0);
+				moveGoomba(p[i].getGround().getX() - getGoombaImg().getX() + getGoombaImg().getWidth(), 0);
 			}
 		}
 	}
@@ -143,7 +159,7 @@ public class Enemies extends GraphicsProgram {
 				}
 				if ((Left.getBounds()).intersects(p[i].getRight().getBounds())) {
 					goombaCollideSide++;
-					moveGoomba(p[i].getGoombaImg().getX() + p[i].getGoombaImg().getWidth() - getGoombaImg().getX(), 0);
+					moveGoomba(p[i].getGoombaImg().getX() - getGoombaImg().getX() + getGoombaImg().getWidth(), 0);
 				}
 			}
 		}
